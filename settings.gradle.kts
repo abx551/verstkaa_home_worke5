70187b295151fc58de2281f7f846728695c3d2b8
abx551 <gniyazowa352@gmail.com>
2024-02-19 18:28:46 +0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "verstkaa_home_worke5"
+include(":app")
